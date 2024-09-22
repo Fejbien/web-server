67a74b2bb26790e8a19a6dc7f74f5f37372191e7
Fabian Sucholas <fabian.sucholas123@gmail.com>
2024-09-22 17:55:18 +0200

    init

--- http_tcpServer.h
+++ http_tcpServer.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef INCLUDED_HTTP_TCPSERVER_LINUX
+#define INCLUDED_HTTP_TCPSERVER_LINUX
+namespace http
+{
+    class TcpServer
+    {
+    public:
+        TcpServer();
+        ~TcpServer();
+    private:
+
+    };
+}
+#endif
---
+++ server.cpp
@@ -0,0 +1,8 @@
+#include "http_tcpServer.h"
+
+int main()
+{
+	http::TcpServer server = http::TcpServer();
+
+	return 0;
+}
